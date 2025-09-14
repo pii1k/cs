d0ba5f3a59dce7ebb9a8100f9b8d56fb965893da
jyan <ajy0535@naver.com>
2025-09-15 03:06:59 +0900

    fix test bug - rust: Ord is not supported for float type since it's NaN

--- refresh-rust/algorithms/tests/sorting_test.rs
+++ refresh-rust/algorithms/tests/sorting_test.rs
@@ -10,15 +10,6 @@ fn selection_sort_test() {
 }
 
 #[test]
-fn selection_sort_test2() {
-    let mut test_vec = [2.1,3.3,5.4,1.1,6.2,4.3,10.2,9.4,7.5,8.6];
-    let expect_result = [1,2,3,4,5,6,7,8,9,10];
-
-    selection_sort(&mut test_vec);
-    assert_eq!(test_vec, expect_result);
-}
-
-#[test]
 fn bubble_sort_test() {
     let mut test_vec = [2,3,5,1,6,4,10,9,7,8];
     let expect_result = [1,2,3,4,5,6,7,8,9,10];
